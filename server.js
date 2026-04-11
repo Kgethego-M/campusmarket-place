15523f43eeef4cc68e2bdea3cf26497346dddcdd
siphokazi-tech6 <s08027456@gmail.com>
2026-04-11 22:12:16 +0200

    Fix Express wildcard route syntax

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.static(join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
