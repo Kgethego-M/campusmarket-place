324ab9c44e996431f17bb3bad7100a0d79497be2
siphokazi-tech6 <s08027456@gmail.com>
2026-04-11 21:51:21 +0200

    Fix server.js to use ES module syntax

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
