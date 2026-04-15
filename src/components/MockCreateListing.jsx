ca3b6dfdb330a0690580941ebe4a6d88d2251276
IcedMallard4334 <hyginusvictor11@gmail.com>
2026-04-15 14:31:02 +0200

    Fixed LOGIN problem

--- src/components/MockCreateListing.jsx
+++ src/components/MockCreateListing.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { auth } from "../firebase.mock.js";
+import { auth } from "../firebase.js";
 
 import {
     validateListing,
